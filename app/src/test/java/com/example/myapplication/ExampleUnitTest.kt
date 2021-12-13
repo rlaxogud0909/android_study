0622a0d672acf161de55267cb25d0c5cf91d920d
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-13 10:36:37 +0900

    피보나치수열을 TDD 구현

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -14,4 +14,18 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun testPibo() {
+        assertEquals(0, pibo(0))
+        assertEquals(1, pibo(1))
+        assertEquals(1, pibo(2))
+        assertEquals(2, pibo(3))
+        assertEquals(3, pibo(4))
+    }
+
+    private fun pibo(n: Int): Int {
+        if (n == 0) return 0
+        if (n <= 2) return 1
+        return pibo(n - 2) + pibo(n - 1)
+    }
 }
